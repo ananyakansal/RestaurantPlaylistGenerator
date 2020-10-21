13305f37ca3eec51c969f62838cfcb544a35d483
jocekav <jkavanagh3@gatech.edu>
2020-10-20 21:11:49 -0400

    Integrate temporary display for demo (very messy html)

--- app.py
+++ app.py
@@ -20,20 +20,35 @@ def callback():
 def player():
     userToken = spotifyAuthentication.getUserToken(request.args['code'])
     token = {'userToken': userToken[0]}
-    # print(token)
-    return render_template('player.html', token=token)
+    print(token)
+    return render_template('temp.html', token=token)
 
 @app.route("/initPlayer")
 def initPlayer():
     spotifyPlayback.initPlayer()
+    return 'nothing'
 
 @app.route("/startPlayback")
 def startPlayback():
-    spotifyPlayback.startPlayback()
+    spotifyPlayback.startPlayback('spotify:track:4N0TP4Rmj6QQezWV88ARNJ')
+    # spotifyPlayback.
+    # spotifyPlayback.startPlayback()
+    return 'nothing'
 
 @app.route("/pausePlayback")
 def pausePlayback():
     spotifyPlayback.pausePlayback()
-    
+    return 'nothing'
+
+@app.route('/addQueue')
+def addQueue():
+    spotifyPlayback.addSongToQueue('spotify:track:5aaUXcrsXI477I93yBE8lu')
+    return 'nothing'
+
+@app.route('/nextTrack')
+def nextTrack():
+    spotifyPlayback.nextSong()
+    return 'nothing'
+
 if __name__ == "__main__":
     app.run(debug=True, port=5000)
--- spotifyPlayback.py
+++ spotifyPlayback.py
@@ -3,7 +3,7 @@ import json
 import spotipy
 from spotipy.oauth2 import SpotifyOAuth
 
-CLIENT_ID = ""
+CLIENT_ID = "cc02566bde9444cdb1aa1855f3fce9d1"
 CLIENT_SECRET = ""
 PORT = "5000"
 CLIENT_URL = "http://127.0.0.1"
@@ -14,7 +14,10 @@ TOKEN_DATA = []
 
 def initPlayer():
     global sp
-    sp = spotipy.Spotify(client_credentials_manager=SpotifyOAuth(scope=SCOPE, client_id=CLIENT_ID, client_secret=CLIENT_SECRET, redirect_uri=REDIRECT_URI))
+    try:
+        sp = spotipy.Spotify(client_credentials_manager=SpotifyOAuth(scope=SCOPE, client_id=CLIENT_ID, client_secret=CLIENT_SECRET, redirect_uri=REDIRECT_URI))
+    except:
+        print('error')
     # sp.transfer_playback(device_id='1360e7e1bf1126627e48ce5c86e08e04697abb5e')
     # devices = sp.devices()['devices']
     # for i in devices:
@@ -26,8 +29,31 @@ def initPlayer():
     DEVICE_ID = sp.devices()['devices'][0]['id']
     sp.transfer_playback(device_id=DEVICE_ID, force_play=False)
 
-def startPlayback():
-    sp.start_playback(uris=['spotify:track:6gdLoMygLsgktydTQ71b15'])
+def startPlayback(uri):
+    progress = sp.current_playback()['progress_ms']
+    sp.start_playback(uris= [uri], position_ms=progress)
 
 def pausePlayback():
     sp.pause_playback()
+
+def addSongToQueue(uri):
+    sp = spotipy.Spotify(client_credentials_manager=SpotifyOAuth(scope=SCOPE, client_id=CLIENT_ID, client_secret=CLIENT_SECRET, redirect_uri=REDIRECT_URI))
+    sp.add_to_queue(uri)
+
+def nextSong():
+    sp.next_track()
+
+def getSpotifyURI(title, artist=''):
+    sp = spotipy.Spotify(client_credentials_manager=SpotifyOAuth(scope=SCOPE, client_id=CLIENT_ID, client_secret=CLIENT_SECRET, redirect_uri=REDIRECT_URI))
+    resp = sp.search(q='track:"{}" artist:"{}"'.format(title, artist),type='track',limit=1)['tracks']['items']
+    # li = resp['tracks']['items']
+    if not resp:
+        return ('Not Found')
+    else:
+        return resp[0]['uri']
+
+# print(getSpotifyURI('Superstition'))
+# sp = spotipy.Spotify(client_credentials_manager=SpotifyOAuth(scope=SCOPE, client_id=CLIENT_ID, client_secret=CLIENT_SECRET, redirect_uri=REDIRECT_URI))
+# DEVICE_ID = sp.devices()['devices'][0]['id']
+# sp.transfer_playback(device_id=DEVICE_ID, force_play=False)
+#addSongToQueue('spotify:track:5aaUXcrsXI477I93yBE8lu')
